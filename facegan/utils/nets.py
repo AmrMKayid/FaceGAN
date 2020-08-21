46c14d20b951ef781bdb22df19b4995623b73e4f
AmrMKayid <amrmkayid@gmail.com>
2020-08-21 17:48:21 +0200

    Downloading Custom StyleGAN2 pretrained networks

--- facegan/utils/nets.py
+++ facegan/utils/nets.py
@@ -5,6 +5,9 @@
 # https://nvlabs.github.io/stylegan2/license.html
 """List of pre-trained StyleGAN2 networks located on Google Drive."""
 
+import os
+import glob
+import gdown
 import pickle
 from facegan import ROOT_PATH
 import facegan.dnnlib as dnnlib
@@ -87,6 +90,52 @@ gdrive_urls = {
     'gdrive:networks/table2/stylegan2-ffhq-config-e-Gskip-Dskip.pkl':
         'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/table2/stylegan2-ffhq-config-e-Gskip-Dskip.pkl',
 }
+
+vgg16 = 'vgg16_zhang_perceptual.pkl'
+model = 'stylegan2-ffhq-config-f.pkl'
+networks_urls = {
+    'default': [
+        'http://d36zk2xti64re0.cloudfront.net/stylegan2/networks/stylegan2-ffhq-config-f.pkl',
+        'stylegan2-ffhq-config-f.pkl'
+    ],
+    'european': [
+        'https://drive.google.com/uc?id=1--kh2Em5U1qh-H7Lin9FzppkZCQ18c4W',
+        'generator_model-stylegan2-config-f.pkl'
+    ],
+    'asian': [
+        'https://drive.google.com/uc?id=1-3XU6KzIVywFoKXx2zG1hW8mH4OYpyO9',
+        'generator_yellow-stylegan2-config-f.pkl'
+    ],
+    'asian beauty': [
+        'https://drive.google.com/uc?id=1-04v78_pI59M0IvhcKxsm3YhK2-plnbj',
+        'generator_star-stylegan2-config-f.pkl'
+    ],
+    'baby': [
+        'https://drive.google.com/uc?id=1--684mANXSgC3aDhLc7lPM7OBHWuVRXa',
+        'generator_baby-stylegan2-config-f.pkl'
+    ]
+}
+
+
+def download_networks(network: str = 'default'):
+  network_url = networks_urls[network][0]
+  network_name = networks_urls[network][1]
+  network_pkl = f'{ROOT_PATH}/data/models/{network_name}'
+  if network == 'default':
+    load_networks('gdrive:networks/stylegan2-ffhq-config-f.pkl')
+    network_path = glob.glob(
+        f'{ROOT_PATH}/data/models/*stylegan2-ffhq-config-f.pkl')[0]
+    print(network_path)
+    os.rename(network_path, network_pkl)
+  else:
+    gdown.download(
+        network_url,
+        network_name,
+        quiet=False,
+    )
+    os.rename(f"{os.getcwd()}/{network_name}", network_pkl)
+  return network_pkl
+
 
 _cached_networks = dict()
 
